483bba92ffd3f2f66b2c506224219c1a63429b0e
Liliana Gorga <gorgaliliana87@gmail.com>
2024-04-03 09:05:05 +0200

    initialize project

---
+++ src/main/java/dev/lilianagorga/wearagain/WearAgainApplication.java
@@ -0,0 +1,13 @@
+package dev.lilianagorga.wearagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearAgainApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WearAgainApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/lilianagorga/wearagain/WearAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lilianagorga.wearagain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearAgainApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
